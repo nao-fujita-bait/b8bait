4dfdb44eb3031319b8161105eb88ae58425dfe21
nao-fujita-bait <46522400+nao-fujita-bait@users.noreply.github.com>
2023-05-17 12:20:08 +0900

    Add files via upload

---
+++ security_check.js
@@ -0,0 +1,8 @@
+    function getValue() {
+      // テキスト入力欄の値を取得
+      var inputElement = document.getElementById("inputText");
+      var value = inputElement.value;
+      value = '<p>'+value+'</p>';
+      // 取得した値を表示
+      document.getElementById("area").innerHTML = value;
+    }
